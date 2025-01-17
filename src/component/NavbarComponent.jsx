3dd977d0204c2f1c5cdfa03a573704ee802edf6c
Fauzi Ferdiansyah <fauziferdiansyahindo@gmail.com>
2025-01-17 20:04:15 +0700

    feat : add navbar steam main

--- src/component/NavbarComponent.jsx
+++ src/component/NavbarComponent.jsx
@@ -2,8 +2,43 @@ import React from "react";
 
 const NavbarComponent = () => {
   return (
-    <div>
-      <h1>Navabar</h1>
+    <div className="flex items-center px-20 py-4 justify-items-center ">
+      <div>
+        <img
+          src="https://store.fastly.steamstatic.com/public/shared/images/header/logo_steam.svg?t=962016"
+          alt="logo"
+          className="w-44"
+        />
+      </div>
+      <div>
+        <ul className="flex items-center gap-3 text-white ps-10">
+          <li>
+            <a href="#" className="hover:font-semibold">
+              STORE
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:font-semibold">
+              COMMUNITYS
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:font-semibold">
+              USER
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:font-semibold">
+              CHAT
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:font-semibold">
+              SUPPORT
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
